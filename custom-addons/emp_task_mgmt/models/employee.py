262487fe943c0a3a0a790e8a9032f88b504908bf
SultanZafar <ranasultan562@gmail.com>
2026-05-14 17:23:15 +0500

    feat: update employee model with new fields

--- custom-addons/emp_task_mgmt/models/employee.py
+++ custom-addons/emp_task_mgmt/models/employee.py
@@ -4,6 +4,29 @@ class EmployeeCustom(models.Model):
     _name = 'hr.employee.custom'
     _description = 'Employee'
 
+    employee_id = fields.Char ('Employee ID', required=True)
     name = fields.Char('Name', required=True)
     department = fields.Char('Department', required=True)
+    phone = fields.Char('Phone')
     email = fields.Char('Email', required=True)
+    designation = fields.Char('Designation')
+    joining_date = fields.Date('Joining Date')
+    location = fields.Char('Location')
+    employment_type = fields.Selection([
+        ('permanent', 'Permanent'),
+        ('contract', 'Contract'),
+        ('frelance', 'Frelance'),
+    ], 'Employee Type')
+    work_mode = fields.Selection([
+        ('onsite', 'On site'),
+        ('remote', 'Remote'),
+        ('hybrid', 'Hybrid'),
+    ], 'Work Mode')
+    status = fields.Selection([
+        ('active', 'Active'),
+        ('inactive', 'Inactive'),
+    ], 'Status', default='active')
+    reporting_manager = fields.Many2one(
+        'hr.employee.custom',
+        'Reporting Manager',
+    )
