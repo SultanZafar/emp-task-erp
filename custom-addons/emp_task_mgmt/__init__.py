b0bf328d50d88ad0a95b847cdb6b65a1c5dd4685
SultanZafar <ranasultan562@gmail.com>
2026-05-23 23:34:36 +0500

    fix: remove trailing blank lines

--- custom-addons/emp_task_mgmt/__init__.py
+++ custom-addons/emp_task_mgmt/__init__.py
@@ -1,5 +1,2 @@
 from . import models  # noqa: F401
 from . import controllers  # noqa: F401
-
-
-
--- custom-addons/emp_task_mgmt/models/__init__.py
+++ custom-addons/emp_task_mgmt/models/__init__.py
@@ -1,6 +1,3 @@
 from . import employee  # noqa: F401
 from . import task  # noqa: F401
 from . import payroll  # noqa: F401
-
-
-
